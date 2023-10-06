8571bd4c7cc71c279bf2696dce92d0975ee7ece5
Bryan Yang <e0772641@u.nus.edu>
2023-10-06 16:19:32 +0800

    Rename layout functions for individual pages

--- frontend/src/app/question/[id]/layout.tsx
+++ frontend/src/app/question/[id]/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import Navbar from '@/components/navBar/NavBar';
 
-export default function AccountLayout({ children }: { children: ReactNode }) {
+export default function QuestionLayout({ children }: { children: ReactNode }) {
   return <Navbar>{children}</Navbar>;
 }
