06e2fe2aef207b21c53dfb2b202e95c5f25f575d
SeekSaveServe <peichengyi@gmail.com>
2023-10-13 13:23:34 +0800

    chore: change to css

--- frontend/src/app/collab/page.tsx
+++ frontend/src/app/collab/page.tsx
@@ -1,9 +1,19 @@
 'use client';
 
-import VideoCollection from '@/components/video/VideoCollection';
+// import VideoCollection from '@/components/video/VideoCollection';
 import { Box } from '@chakra-ui/react';
+// import { useEffect } from 'react';
+
+import dynamic from 'next/dynamic';
 
 function Page() {
+  const VideoCollection = dynamic(
+    () => import('@/components/video/VideoCollection'),
+    {
+      ssr: false,
+    },
+  );
+
   return (
     <Box>
       <VideoCollection roomId="1" partnerUsername="Test" />{' '}
