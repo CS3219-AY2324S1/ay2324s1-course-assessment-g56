02d0136f1417062951033551f731d154fecbd788
Linus <linuslee2000@gmail.com>
2023-11-03 15:07:01 +0800

    update env variables

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
     QUESTION_PATH: process.env.QUESTION_PATH,
     USER_PATH: process.env.USER_PATH,
     MATCHING_PATH: process.env.MATCHING_PATH,
+    VIDEO_PATH: process.env.VIDEO_PATH,
     SUPABASE_URL: process.env.SUPABASE_URL,
     SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
   },
