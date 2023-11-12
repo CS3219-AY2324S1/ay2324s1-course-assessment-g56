4f8eec596fe1431b38525689c5daa55c858224fb
LowJiaHao99 <77181204+LowJiaHao99@users.noreply.github.com>
2023-11-12 22:45:00 +0800

    Remove room route in frontend

--- frontend/src/app/room/[slug]/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from 'react';
-import Navbar from '@/components/navBar/NavBar';
-
-export default function RoomLayout({ children }: { children: ReactNode }) {
-  return <Navbar>{children}</Navbar>;
-}
