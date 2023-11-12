4feaaefc97d3bffd0e9d6e23cc92d42923aec2f1
LowJiaHao99 <77181204+LowJiaHao99@users.noreply.github.com>
2023-11-12 22:18:00 +0800

    Remove files not used in assignment 2
    Remove gke-cluster directory
    Dockerfiles and dockerignore for frontend, question, matching and user service
    remove backend/matching directory

--- gke-cluster/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Secrets and configmaps
-kubectl delete ingress --all
-kubectl delete deployment --all
-kubectl delete service --all
-kubectl delete configmap --all
-kubectl delete secret --all
-kubectl delete pod --all
