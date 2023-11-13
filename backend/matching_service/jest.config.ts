63c89519a852608f2b231c4ab207b3a9e88a28b0
LowJiaHao99 <77181204+LowJiaHao99@users.noreply.github.com>
2023-11-13 15:43:51 +0800

    Update jest.config.ts for matching_service

--- backend/matching_service/jest.config.ts
+++ backend/matching_service/jest.config.ts
@@ -11,9 +11,13 @@ const config: Config.InitialOptions = {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   testMatch: ['**/*.test.ts'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/src',
-  }),
+  moduleNameMapper: {
+    '^constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^socket/(.*)$': '<rootDir>/src/socket/$1',
+    '^struct/(.*)$': '<rootDir>/src/struct/$1',
+    '^types/(.*)$': '<rootDir>/src/types/$1',
+    '^mockComponents/(.*)$': '<rootDir>/src/mockComponents/$1',
+  },
 };
 
 export default config;
