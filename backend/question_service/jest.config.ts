ee430dcb98edf389565fc5c5642c9873de3e3813
LowJiaHao99 <77181204+LowJiaHao99@users.noreply.github.com>
2023-11-13 15:30:56 +0800

    Update jest.config in question_service

--- backend/question_service/jest.config.ts
+++ backend/question_service/jest.config.ts
@@ -8,10 +8,10 @@ const config: Config.InitialOptions = {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   moduleNameMapper: {
-    '^controllers/(.*)$': '<rootDir>/controllers/$1',
-    '^middleware/(.*)$': '<rootDir>/middleware/$1',
-    '^routes/(.*)$': '<rootDir>/routes/$1',
-    '^types/(.*)$': '<rootDir>/types/$1',
+    '^controllers/(.*)$': '<rootDir>/src/controllers/$1',
+    '^middleware/(.*)$': '<rootDir>/src/middleware/$1',
+    '^routes/(.*)$': '<rootDir>/src/routes/$1',
+    '^types/(.*)$': '<rootDir>/src/types/$1',
   },
 };
 
