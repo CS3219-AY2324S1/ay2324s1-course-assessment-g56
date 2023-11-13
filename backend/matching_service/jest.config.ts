8ec496b479986aa2c4ab40592ae27d87e84b2b57
LowJiaHao99 <77181204+LowJiaHao99@users.noreply.github.com>
2023-11-13 15:55:35 +0800

    Remove imports not used in jest.config.ts

--- backend/matching_service/jest.config.ts
+++ backend/matching_service/jest.config.ts
@@ -1,7 +1,4 @@
 import type { Config } from '@jest/types';
-import { pathsToModuleNameMapper } from 'ts-jest';
-
-import { compilerOptions } from './tsconfig.json';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
